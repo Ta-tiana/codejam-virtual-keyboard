4fd9d6f3b4f515900c25ff0d2af4b28a44693058
Ta-tiana <ustinya88@gmail.com>
2019-11-05 23:59:39 +0300

    feat: add script with keys data

---
+++ assets/scripts/keys.js
@@ -0,0 +1,167 @@
+
+// eslint-disable-next-line no-unused-vars
+const keysRow1 = [
+  ['Backquote', '', 'ru', 'ё', 'Ё', '', 'eng', '`', '~'],
+  ['Digit1', '', 'ru', '1', '!', '', 'eng', '1', '!'],
+  ['Digit2', '', 'ru', '2', '"', '', 'eng', '2', '@'],
+  ['Digit3', '', 'ru', '3', '№', '', 'eng', '3', '#'],
+  ['Digit4', '', 'ru', '4', ';', '', 'eng', '4', '$'],
+  ['Digit5', '', 'ru', '5', '%', '', 'eng', '5', '%'],
+  ['Digit6', '', 'ru', '6', ':', '', 'eng', '6', '^'],
+  ['Digit7', '', 'ru', '7', '?', '', 'eng', '7', '&'],
+  ['Digit8', '', 'ru', '8', '*', '', 'eng', '8', '*'],
+  ['Digit9', '', 'ru', '9', '(', '', 'eng', '9', '('],
+  ['Digit0', '', 'ru', '0', ')', '', 'eng', '0', ')'],
+  ['Minus', '', 'ru', '-', '_', '', 'eng', '-', '_'],
+  ['Equal', '', 'ru', '=', '+', '', 'eng', '=', '+'],
+  ['Backspace', 'backspace', 'ru', 'Backspace', 'Backspace', 'backspace', 'eng', 'Backspace', 'Backspace'],
+];
+
+// eslint-disable-next-line no-unused-vars
+const keysRow2 = [
+  ['Tab', 'tab', 'ru', 'Tab', 'Tab', 'tab', 'eng', 'Tab', 'Tab'],
+  ['KeyQ', '', 'ru', 'й', 'Й', '', 'eng', 'q', 'Q'],
+  ['KeyW', '', 'ru', 'ц', 'Ц', '', 'eng', 'w', 'W'],
+  ['KeyE', '', 'ru', 'у', 'У', '', 'eng', 'e', 'E'],
+  ['KeyR', '', 'ru', 'к', 'К', '', 'eng', 'r', 'R'],
+  ['KeyT', '', 'ru', 'е', 'Е', '', 'eng', 't', 'T'],
+  ['KeyY', '', 'ru', 'н', 'Н', '', 'eng', 'y', 'Y'],
+  ['KeyU', '', 'ru', 'г', 'Г', '', 'eng', 'u', 'U'],
+  ['KeyI', '', 'ru', 'ш', 'Ш', '', 'eng', 'i', 'I'],
+  ['KeyO', '', 'ru', 'щ', 'Щ', '', 'eng', 'o', 'O'],
+  ['KeyP', '', 'ru', 'з', 'З', '', 'eng', 'p', 'P'],
+  ['BracketLeft', '', 'ru', 'х', 'Х', '', 'eng', '[', '{'],
+  ['BracketRight', '', 'ru', 'ъ', 'Ъ', '', 'eng', ']', '}'],
+  ['Backslash', '', 'ru', '\\', '/', '', 'eng', '\\', '|'],
+  ['Delete', 'del', 'ru', 'DEL', 'DEL', 'del', 'eng', 'DEL', 'DEL'],
+];
+
+// eslint-disable-next-line no-unused-vars
+const keysRow3 = [
+  ['CapsLock', 'capslock', 'ru', 'CapsLock', 'CapsLock', 'capslock', 'eng', 'CapsLock', 'CapsLock'],
+  ['KeyA', '', 'ru', 'ф', 'Ф', '', 'eng', 'a', 'A'],
+  ['KeyS', '', 'ru', 'ы', 'Ы', '', 'eng', 's', 'S'],
+  ['KeyD', '', 'ru', 'в', 'В', '', 'eng', 'd', 'D'],
+  ['KeyF', '', 'ru', 'а', 'А', '', 'eng', 'f', 'F'],
+  ['KeyG', '', 'ru', 'п', 'П', '', 'eng', 'g', 'G'],
+  ['KeyH', '', 'ru', 'р', 'Р', '', 'eng', 'h', 'H'],
+  ['KeyJ', '', 'ru', 'о', 'О', '', 'eng', 'j', 'J'],
+  ['KeyK', '', 'ru', 'л', 'Л', '', 'eng', 'k', 'K'],
+  ['KeyL', '', 'ru', 'д', 'Д', '', 'eng', 'l', 'L'],
+  ['Semicolon', '', 'ru', 'ж', 'Ж', '', 'eng', ';', ':'],
+  ['Quote', '', 'ru', 'э', 'Э', '', 'eng', "'", '"'],
+  ['Enter', 'enter', 'ru', 'ENTER', 'ENTER', 'enter', 'eng', 'ENTER', 'ENTER'],
+];
+
+// eslint-disable-next-line no-unused-vars
+const keysRow4 = [
+  ['ShiftLeft', 'shift', 'ru', 'Shift', 'Shift', 'shift', 'eng', 'Shift', 'Shift'],
+  ['KeyZ', '', 'ru', 'я', 'Я', '', 'eng', 'z', 'Z'],
+  ['KeyX', '', 'ru', 'ч', 'Ч', '', 'eng', 'x', 'X'],
+  ['KeyC', '', 'ru', 'с', 'С', '', 'eng', 'c', 'C'],
+  ['KeyV', '', 'ru', 'м', 'М', '', 'eng', 'v', 'V'],
+  ['KeyB', '', 'ru', 'и', 'И', '', 'eng', 'b', 'B'],
+  ['KeyN', '', 'ru', 'т', 'Т', '', 'eng', 'n', 'N'],
+  ['KeyM', '', 'ru', 'ь', 'Ь', '', 'eng', 'm', 'M'],
+  ['Comma', '', 'ru', 'б', 'Б', '', 'eng', ',', '<'],
+  ['Period', '', 'ru', 'ю', 'Ю', '', 'eng', '.', '>'],
+  ['Slash', '', 'ru', '.', ',', '', 'eng', '/', '?'],
+  ['ArrowUp', 'arrow', 'ru', '▲', '▲', 'arrow', 'eng', '▲', '▲'],
+  ['ShiftRight', 'shift-right', 'ru', 'Shift', 'Shift', 'shift-right', 'eng', 'Shift', 'Shift'],
+];
+
+// eslint-disable-next-line no-unused-vars
+const keysRow5 = [
+  ['ControlLeft', 'ctrl', 'ru', 'Ctrl', 'Ctrl', 'ctrl', 'eng', 'Ctrl', 'Ctrl'],
+  ['MetaLeft', 'win', 'ru', 'Win', 'Win', 'win', 'eng', 'Win', 'Win'],
+  ['AltLeft', 'alt', 'ru', 'Alt', 'Alt', 'alt', 'eng', 'Alt', 'Alt'],
+  ['Space', 'space', 'ru', ' ', ' ', 'space', 'eng', ' ', ' '],
+  ['AltRight', 'alt', 'ru', 'Alt', 'Alt', 'alt', 'eng', 'Alt', 'Alt'],
+  ['ArrowLeft', 'arrow', 'ru', '◄', '◄', 'arrow', 'eng', '◄', '◄'],
+  ['ArrowDown', 'arrow', 'ru', '▼', '▼', 'arrow', 'eng', '▼', '▼'],
+  ['ArrowRight', 'arrow', 'ru', '►', '►', 'arrow', 'eng', '►', '►'],
+  ['ControlRight', 'ctrl', 'ru', 'Ctrl', 'Ctrl', 'ctrl', 'eng', 'Ctrl', 'Ctrl'],
+];
+
+// const keysRow1 = [
+//   ['Backquote', '', 'ru hidden', 'ё', 'Ё', '', 'eng', '`', '~'],
+//   ['Digit1', '', 'ru hidden', '1', '!', '', 'eng', '1', '!'],
+//   ['Digit2', '', 'ru hidden', '2', '"', '', 'eng', '2', '@'],
+//   ['Digit3', '', 'ru hidden', '3', '№', '', 'eng', '3', '#'],
+//   ['Digit4', '', 'ru hidden', '4', ';', '', 'eng', '4', '$'],
+//   ['Digit5', '', 'ru hidden', '5', '%', '', 'eng', '5', '%'],
+//   ['Digit6', '', 'ru hidden', '6', ':', '', 'eng', '6', '^'],
+//   ['Digit7', '', 'ru hidden', '7', '?', '', 'eng', '7', '&'],
+//   ['Digit8', '', 'ru hidden', '8', '*', '', 'eng', '8', '*'],
+//   ['Digit9', '', 'ru hidden', '9', '(', '', 'eng', '9', '('],
+//   ['Digit0', '', 'ru hidden', '0', ')', '', 'eng', '0', ')'],
+//   ['Minus', '', 'ru hidden', '-', '_', '', 'eng', '-', '_'],
+//   ['Equal', '', 'ru hidden', '=', '+', '', 'eng', '=', '+'],
+//   ['Backspace', 'backspace', 'ru hidden', 'Backspace', 'Backspace', 'backspace', 'eng', 'Backspace', 'Backspace'],
+// ];
+
+// eslint-disable-next-line no-unused-vars
+// const keysRow2 = [
+//   ['Tab', 'tab', 'ru hidden', 'Tab', 'Tab', 'tab', 'eng', 'Tab', 'Tab'],
+//   ['KeyQ', '', 'ru hidden', 'й', 'Й', '', 'eng', 'q', 'Q'],
+//   ['KeyW', '', 'ru hidden', 'ц', 'Ц', '', 'eng', 'w', 'W'],
+//   ['KeyE', '', 'ru hidden', 'у', 'У', '', 'eng', 'e', 'E'],
+//   ['KeyR', '', 'ru hidden', 'к', 'К', '', 'eng', 'r', 'R'],
+//   ['KeyT', '', 'ru hidden', 'е', 'Е', '', 'eng', 't', 'T'],
+//   ['KeyY', '', 'ru hidden', 'н', 'Н', '', 'eng', 'y', 'Y'],
+//   ['KeyU', '', 'ru hidden', 'г', 'Г', '', 'eng', 'u', 'U'],
+//   ['KeyI', '', 'ru hidden', 'ш', 'Ш', '', 'eng', 'i', 'I'],
+//   ['KeyO', '', 'ru hidden', 'щ', 'Щ', '', 'eng', 'o', 'O'],
+//   ['KeyP', '', 'ru hidden', 'з', 'З', '', 'eng', 'p', 'P'],
+//   ['BracketLeft', '', 'ru hidden', 'х', 'Х', '', 'eng', '[', '{'],
+//   ['BracketRight', '', 'ru hidden', 'ъ', 'Ъ', '', 'eng', ']', '}'],
+//   ['Backslash', '', 'ru hidden', '\\', '/', '', 'eng', '\\', '|'],
+//   ['Delete', 'del', 'ru hidden', 'DEL', 'DEL', 'del', 'eng', 'DEL', 'DEL'],
+// ];
+//
+// // eslint-disable-next-line no-unused-vars
+// const keysRow3 = [
+//   ['CapsLock', 'capslock', 'ru hidden', 'CapsLock', 'CapsLock', 'capslock', 'eng', 'CapsLock', 'CapsLock'],
+//   ['KeyA', '', 'ru hidden', 'ф', 'Ф', '', 'eng', 'a', 'A'],
+//   ['KeyS', '', 'ru hidden', 'ы', 'Ы', '', 'eng', 's', 'S'],
+//   ['KeyD', '', 'ru hidden', 'в', 'В', '', 'eng', 'd', 'D'],
+//   ['KeyF', '', 'ru hidden', 'а', 'А', '', 'eng', 'f', 'F'],
+//   ['KeyG', '', 'ru hidden', 'п', 'П', '', 'eng', 'g', 'G'],
+//   ['KeyH', '', 'ru hidden', 'р', 'Р', '', 'eng', 'h', 'H'],
+//   ['KeyJ', '', 'ru hidden', 'о', 'О', '', 'eng', 'j', 'J'],
+//   ['KeyK', '', 'ru hidden', 'л', 'Л', '', 'eng', 'k', 'K'],
+//   ['KeyL', '', 'ru hidden', 'д', 'Д', '', 'eng', 'l', 'L'],
+//   ['Semicolon', '', 'ru hidden', 'ж', 'Ж', '', 'eng', ';', ':'],
+//   ['Quote', '', 'ru hidden', 'э', 'Э', '', 'eng', "'", '"'],
+//   ['Enter', 'enter', 'ru hidden', 'ENTER', 'ENTER', 'enter', 'eng', 'ENTER', 'ENTER'],
+// ];
+//
+// // eslint-disable-next-line no-unused-vars
+// const keysRow4 = [
+//   ['ShiftLeft', 'shift', 'ru hidden', 'Shift', 'Shift', 'shift', 'eng', 'Shift', 'Shift'],
+//   ['KeyZ', '', 'ru hidden', 'я', 'Я', '', 'eng', 'z', 'Z'],
+//   ['KeyX', '', 'ru hidden', 'ч', 'Ч', '', 'eng', 'x', 'X'],
+//   ['KeyC', '', 'ru hidden', 'с', 'С', '', 'eng', 'c', 'C'],
+//   ['KeyV', '', 'ru hidden', 'м', 'М', '', 'eng', 'v', 'V'],
+//   ['KeyB', '', 'ru hidden', 'и', 'И', '', 'eng', 'b', 'B'],
+//   ['KeyN', '', 'ru hidden', 'т', 'Т', '', 'eng', 'n', 'N'],
+//   ['KeyM', '', 'ru hidden', 'ь', 'Ь', '', 'eng', 'm', 'M'],
+//   ['Comma', '', 'ru hidden', 'б', 'Б', '', 'eng', ',', '<'],
+//   ['Period', '', 'ru hidden', 'ю', 'Ю', '', 'eng', '.', '>'],
+//   ['Slash', '', 'ru hidden', '.', ',', '', 'eng', '/', '?'],
+//   ['ArrowUp', 'arrow', 'ru hidden', '▲', '▲', 'arrow', 'eng', '▲', '▲'],
+//   ['ShiftRight', 'shift-right', 'ru hidden', 'Shift', 'Shift', 'shift-right', 'eng', 'Shift', 'Shift'],
+// ];
+//
+// // eslint-disable-next-line no-unused-vars
+// const keysRow5 = [
+//   ['CtrlLeft', 'ctrl', 'ru hidden', 'Ctrl', 'Ctrl', 'ctrl', 'eng', 'Ctrl', 'Ctrl'],
+//   ['MetaLeft', 'win', 'ru hidden', 'Win', 'Win', 'win', 'eng', 'Win', 'Win'],
+//   ['AltLeft', 'alt', 'ru hidden', 'Alt', 'Alt', 'alt', 'eng', 'Alt', 'Alt'],
+//   ['Space', 'space', 'ru hidden', ' ', ' ', 'space', 'eng', ' ', ' '],
+//   ['AltRight', 'alt', 'ru hidden', 'Alt', 'Alt', 'alt', 'eng', 'Alt', 'Alt'],
+//   ['ArrowLeft', 'arrow', 'ru hidden', '◄', '◄', 'arrow', 'eng', '◄', '◄'],
+//   ['ArrowDown', 'arrow', 'ru hidden', '▼', '▼', 'arrow', 'eng', '▼', '▼'],
+//   ['ArrowRight', 'arrow', 'ru hidden', '►', '►', 'arrow', 'eng', '►', '►'],
+//   ['CtrlRight', 'ctrl', 'ru hidden', 'Ctrl', 'Ctrl', 'ctrl', 'eng', 'Ctrl', 'Ctrl'],
+// ];
